8526fccef2ae2aace6af7348e321bea6819ce92c
jenny rolfe <jenny23@gmail.com>
2019-11-26 09:26:14 +0000

    simplify to try to get working

--- autoreply.py
+++ autoreply.py
@@ -5,20 +5,21 @@ import tweepy
 import logging
 import time
 import sys
+import os
 
 from generate_advertisement import get_ad
 #from credentials import *  # use this one for testing
 
 # use this for production; set vars in heroku dashboard
-from os import environ
-CONSUMER_KEY = environ['CONSUMER_KEY']
-CONSUMER_SECRET = environ['CONSUMER_SECRET']
-ACCESS_KEY = environ['ACCESS_KEY']
-ACCESS_SECRET = environ['ACCESS_SECRET']
+
+CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
+CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
+ACCESS_KEY = os.environ.get('ACCESS_KEY')
+ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 
 INTERVAL = 60 * 60 * 6  # tweet every 6 hours
 #INTERVAL = 15  # every 15 seconds, for testing
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 
 def check_mentions(api, keywords, since_id):
@@ -44,16 +45,16 @@ def check_mentions(api, keywords, since_id):
 def main():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    api = tweepy.API(auth)
     print("about to get ad...")
     ad = get_ad()
     api.update_status(ad)
-    time.sleep(INTERVAL)
-    since_id = 1
-    while True:
-        since_id = check_mentions(api, ["hello", "shut up", "hullo", "hi", "hiya"], since_id)
-        logger.info("Waiting...")
-        time.sleep(60)
+    #time.sleep(INTERVAL)
+    #since_id = 1
+    #while True:
+    #    since_id = check_mentions(api, ["hello", "shut up", "hullo", "hi", "hiya"], since_id)
+    #    logger.info("Waiting...")
+    #    time.sleep(300)
 
 if __name__ == "__main__":
     main()
